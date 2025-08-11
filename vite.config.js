b2315a76accc9874858d4f7e7586a8d998270803
kavdiagarv <garvkavdia@gmail.com>
2025-08-11 18:41:23 +0530

    config changed

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: [
+      'ec2-3-6-152-103.ap-south-1.compute.amazonaws.com'
+    ],
+    port: 5173
+  }
+});
+
 
 // https://vite.dev/config/
-export default defineConfig({
-   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [react(), tailwindcss()],
-})
+// export default defineConfig({
+//    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [react(), tailwindcss()],
+// })
